ccfe89e97a049decfb101ac62caddc38e2b0fd94
mansour <115903202+Mansourkareem@users.noreply.github.com>
2024-11-14 13:04:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 #قراءة الملف 
-file_path = 'KSA.xlsx'
+file_path = 'C:/Users/manso/OneDrive/سطح المكتب/inventory system/KSA.xlsx'
 
 
 # التحقق من وجود DataFrame في session_state
